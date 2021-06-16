d4c68bd5737fda5db24aff34c7d1e282df4687c6
danielalvarado96 <44157379+danielalvarado96@users.noreply.github.com>
2021-06-15 18:33:01 -0600

    update

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -26,7 +26,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'templates.xml',
+        'views/view.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
